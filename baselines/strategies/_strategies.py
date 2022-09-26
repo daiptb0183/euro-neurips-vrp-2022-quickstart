cad3190c9888e443f29e41eb7f124c3c362b019e
Dai Pham <daiptb0183@gmail.com>
2022-09-26 12:00:34 +0200

    update somewhat working code greedy

--- baselines/strategies/_strategies.py
+++ baselines/strategies/_strategies.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import numpy as np
 from environment import State
 
@@ -64,10 +65,33 @@ def _dqn(observation: State, rng: np.random.Generator, net):
     return _filter_instance(epoch_instance, mask)
 
 
+def _somewhat_greedy(observation: State, rng: np.random.Generator):
+    mask = np.copy(observation['must_dispatch'])
+    greedy_ratio = (observation["observation"]["current_epoch"]+1) /  observation["static_info"]["num_epochs"]
+    number_of_pending_request = (observation["is_depot"].shape[0]-1)
+    n_request_to_dispatch = int(greedy_ratio*number_of_pending_request)
+    # sorted_idx = np.argsort(observation["time_windows"][:,0])[::-1]
+    sorted_idx_decrease_distance_to_depot = np.argsort(observation["duration_matrix"][0])[::-1]
+    sorted_idx_decrease_tw_closeness = np.argsort(observation["time_windows"][:,0])[::-1]
+    sorted_idx_decrease_demands = np.argsort(observation["demands"])[::-1]
+
+    from copy import deepcopy
+    request_difficulty = deepcopy(observation["request_idx"])
+    for idx in range(request_difficulty.shape[0]):
+        request_difficulty[idx] = np.where(sorted_idx_decrease_distance_to_depot==idx)[0][0] + (1/10)*np.where(sorted_idx_decrease_tw_closeness==idx)[0][0] + (1/10)*np.where(sorted_idx_decrease_demands==idx)[0][0]
+    sorted_idx = np.argsort(request_difficulty)
+
+    mask[sorted_idx[:n_request_to_dispatch]] = True
+    mask[0] = True
+    return _filter_instance(observation, mask)
+
+
+
 STRATEGIES = dict(
     greedy=_greedy,
     lazy=_lazy,
     random=_random,
     supervised=_supervised,
+    somewhat_greedy = _somewhat_greedy,
     dqn=_dqn
 )
